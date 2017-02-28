0b7876d06faadfe7dc2505924bf0421930eb87bb
Ilya Kasnacheev <ikasnacheev@griddynamics.com>
2017-02-27 21:29:35 -0800

    Split into populations.

--- src/main/java/ru/lj/alamar/brainiac/World.java
+++ src/main/java/ru/lj/alamar/brainiac/World.java
@@ -9,83 +9,102 @@ import ru.yandex.bolts.collection.ListF;
 
 public class World {
 
-    private ListF<Hominin> fates;
+    private ListF<ListF<Hominin>> tribes;
     private Random r;
     private int population;
     private int gamePerHunter;
+    private int splitOn;
 
     public World(Properties model, Random r) {
         this.r = r;
-        this.fates = Cf.arrayList();
+        this.tribes = Cf.arrayList();
         this.population = Integer.parseInt(model.getProperty("population"));
         this.gamePerHunter = Integer.parseInt(model.getProperty("game"));
+        this.splitOn = Integer.parseInt(model.getProperty("split.on"));
+        ListF<Hominin> fates = Cf.arrayList();
         for (int i = 0; i < population; i++) {
             fates.add(Hominin.create());
         }
+        tribes.add(fates);
     }
 
     public String advance() {
-        //ListF<Hominin> nextStepFates = Cf.arrayList();
-        ListF<Hominin> underAge = Cf.arrayList();
-        ListF<Hominin> expectant = Cf.arrayList();
-        ListF<Hominin> hunters = Cf.arrayList();
-        ListF<Hominin> freeriders = Cf.arrayList();
+        ListF<ListF<Hominin>> nextStepTribes = Cf.arrayList();
 
-        for (Hominin person : fates.shuffle()) {
-            if (!person.liveOn(r)) {
-                // Dead
-            } else if (person.underAge()) {
-                underAge.add(person);
-            } else if (person.willReproduce(r)) {
-                expectant.add(person);
-            } else if (person.trigger(r, Trait.FREERIDE_AFF)) {
-                freeriders.add(person);
-            } else {
-                hunters.add(person);
-            }
-        }
-
-        ListF<Hominin> newBorn = Cf.arrayList();
-        {
-            ListF<Hominin> parents = expectant;
-            while (parents.length() > 1) {
-                newBorn.add(Hominin.reproduce(r, parents.last(),
-                        parents.get(parents.length() - 2)));
-                parents = parents.rdrop(2);
-            }
-        }
-
-        float totalHuntingSkill = 0f;
-        ListF<Hominin> punished = Cf.arrayList();
-        for (Hominin person : hunters) {
-            if (person.trigger(r, Trait.DETECT_AFF)) {
-                person.spend(1);
-                if (person.trigger(r, Trait.DETECT_SKL) && freeriders.isNotEmpty()) {
-                    punished.add(freeriders.last());
-                    freeriders.rdrop(1);
+        int population = 0;
+        for (ListF<Hominin> fates : tribes) {
+            ListF<Hominin> underAge = Cf.arrayList();
+            ListF<Hominin> expectant = Cf.arrayList();
+            ListF<Hominin> hunters = Cf.arrayList();
+            ListF<Hominin> freeriders = Cf.arrayList();
+            for (Hominin person : fates) {
+                if (!person.liveOn(r)) {
+                    // Dead
+                } else if (person.underAge()) {
+                    underAge.add(person);
+                } else if (person.willReproduce(r)) {
+                    expectant.add(person);
+                } else if (person.trigger(r, Trait.FREERIDE_AFF)) {
+                    freeriders.add(person);
+                } else {
+                    hunters.add(person);
                 }
             }
-            person.spend(2);
-            totalHuntingSkill += person.trait(Trait.HUNT_SKL);
-        }
 
-        int game = (int) (gamePerHunter * totalHuntingSkill);
-
-        ListF<Hominin> eaters = hunters.plus(expectant).plus(freeriders).plus(underAge);
-        do {
-            for (Hominin eater : eaters) {
-                eater.spend(-1);
-                if (--game == 0) {
-                    break;
+            ListF<Hominin> newBorn = Cf.arrayList();
+            {
+                ListF<Hominin> parents = expectant;
+                while (parents.length() > 1) {
+                    newBorn.add(Hominin.reproduce(r, parents.last(),
+                            parents.get(parents.length() - 2)));
+                    parents = parents.rdrop(2);
                 }
             }
-        } while (game > 0);
 
-        fates = eaters.plus(punished).plus(newBorn);
-        for (Hominin hominin : fates) {
-            hominin.spend(Hominin.TURN_COST);
+            float totalHuntingSkill = 0f;
+            ListF<Hominin> punished = Cf.arrayList();
+            for (Hominin person : hunters) {
+                if (person.trigger(r, Trait.DETECT_AFF)) {
+                    person.spend(1);
+                    if (person.trigger(r, Trait.DETECT_SKL) && freeriders.isNotEmpty()) {
+                        punished.add(freeriders.last());
+                        freeriders = freeriders.rdrop(1);
+                    }
+                }
+                person.spend(2);
+                totalHuntingSkill += person.trait(Trait.HUNT_SKL);
+            }
+
+            int game = 10 + (int) (gamePerHunter * totalHuntingSkill);
+
+            ListF<Hominin> eaters = hunters.plus(expectant).plus(freeriders).plus(underAge);
+            while (game > 0 && eaters.isNotEmpty()) {
+                for (Hominin eater : eaters) {
+                    eater.spend(-1);
+                    if (--game == 0) {
+                        break;
+                    }
+                }
+            }
+
+            fates = eaters.plus(punished).plus(newBorn);
+            for (Hominin hominin : fates) {
+                hominin.spend(Hominin.TURN_COST);
+            }
+
+            fates = fates.shuffle();
+            population += fates.size();
+
+            if (fates.size() > splitOn) {
+                int splitAt = 1 + r.nextInt(splitOn - 2);
+                nextStepTribes.add(fates.take(splitAt));
+                nextStepTribes.add(fates.drop(splitAt));
+            } else if (fates.isNotEmpty()) {
+                nextStepTribes.add(fates);
+            }
         }
-        return "population size: " + fates.size();
+        this.tribes = nextStepTribes;
+        return "population " + population + " in " + tribes.length() + " tribes";
     }
 
     private static Hominin selectOther(Random r, ListF<Hominin> fates, Hominin person) {
@@ -100,11 +119,11 @@ public class World {
     }
 
     public ListF<Hominin> getFates() {
-        return fates.unmodifiable();
+        return tribes.<Hominin>flatten();
     }
 
     public boolean isEmpty() {
-        return fates.size() <= 1;
+        return tribes.size() <= 1;
     }
 
 }
